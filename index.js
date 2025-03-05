d811ad9299471da8a40530de5f12f867b7abf4ef
Fahiye Muhammad <fahiye254@gmail.com>
2025-03-05 12:21:34 +0300

    Testing arithmetic operation in Javascript

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2; 
+const multiply = (num1 * num2);
+
+const random = Math.floor(Math.random()) + 1;
+
+const num3 = 10;
+const num4 = 6;
+const mod = (num3 % num4);
+
+const num = (10,12,14,16,18,20)
+
+const max = Math.max(num)
+
+
